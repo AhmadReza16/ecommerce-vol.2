d2b1aa3674adf87f644d6ef86a337f8cbbef8ff2
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-11-05 15:24:11 +0330

    update users app

--- backend/ecommerce/users/admin.py
+++ backend/ecommerce/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Account, UserProfile
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', 'date_joined')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address')
+    search_fields = ('user__email', 'user__username', 'address')
