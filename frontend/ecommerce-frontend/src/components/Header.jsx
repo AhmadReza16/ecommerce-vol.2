6338338bf12c5632269880f159900824f365b342
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-11-22 23:11:22 +0330

    update code

--- frontend/ecommerce-frontend/src/components/Header.jsx
+++ frontend/ecommerce-frontend/src/components/Header.jsx
@@ -3,10 +3,13 @@ import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { useAuth } from "../context/AuthContext";
 import { useCart } from "../context/CartContext";
 import ThemeSwitcher from "./ThemeSwitcher";
+import { useState } from "react";
 
 const Header = () => {
   const { user, logout } = useAuth();
   const { cart } = useCart();
+
+  const [showConfirm, setShowConfirm] = useState(false);
 
   const totalItems = (cart || []).reduce(
     (sum, item) => sum + (item.quantity || 0),
@@ -54,12 +57,36 @@ const Header = () => {
 
           {/* احراز هویت */}
           {user ? (
-            <button
-              onClick={logout}
-              className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition font-serif dark:bg-red-700 dark:hover:bg-red-600"
-            >
-              Logout
-            </button>
+            <div className="relative">
+              <button
+                onClick={() => setShowConfirm((prev) => !prev)}
+                className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition font-serif dark:bg-red-700 dark:hover:bg-red-600"
+              >
+                Logout
+              </button>
+              {showConfirm && (
+                <div className="absolute right-0 top-full mt-2 bg-white dark:bg-gray-800 p-3 rounded-lg shadow-lg border dark:border-gray-700 w-48 z-50">
+                  <p className="text-sm text-gray-700 dark:text-gray-200">
+                    Are You Sure?
+                  </p>
+                  <div className="flex justify-between mt-3">
+                    <button
+                      onClick={logout}
+                      className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700 text-sm"
+                    >
+                      Yes
+                    </button>
+
+                    <button
+                      onClick={() => setShowConfirm(false)}
+                      className="px-3 py-1 bg-gray-300 dark:bg-gray-600 text-gray-800 dark:text-gray-200 rounded hover:bg-gray-400 dark:hover:bg-gray-500 text-sm"
+                    >
+                      No
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
           ) : (
             <Link
               to="/login"
