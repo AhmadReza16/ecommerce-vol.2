beda76482c757fbb945649bbb8251cd465c28aaf
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-11-25 15:37:33 +0330

    update products app admin.py

--- backend/ecommerce/products/admin.py
+++ backend/ecommerce/products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from .models import Product , Category
 
 @admin.register(Product)
@@ -6,6 +7,34 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description')
+
+    readonly_fields = ("image_preview",)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="120" style="border-radius:8px"/>')
+        return "No image"
+
+    fieldsets = (
+        ("Product Info", {
+            "fields": ("name", "category", "price", "stock")
+        }),
+        ("Description", {
+            "fields": ("description",)
+        }),
+        ("Image", {
+            "fields": ("image", "image_preview")
+        }),
+    )
+
+    # جلوگیری از حذف اشتباهی عکس
+    def save_model(self, request, obj, form, change):
+        if change:  
+            old_obj = Product.objects.get(pk=obj.pk)
+            if not obj.image:  
+                obj.image = old_obj.image  
+        super().save_model(request, obj, form, change)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
