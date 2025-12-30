cc595db564a6c6cd9c567496dab3fa0ecc1fb0de
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-12-30 18:39:06 +0330

    add code for adminpanel/admin.py

--- backend/ecommerce/adminpanel/admin.py
+++ backend/ecommerce/adminpanel/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import adminAccount
 
-# Register your models here.
+
+@admin.register(adminAccount)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', 'date_joined')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+
